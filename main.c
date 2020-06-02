85acaf9d064d3abe428b7f26aacc56d8d30ee3aa
Sambor Gogacz <6859944+esgie@users.noreply.github.com>
2020-06-02 16:59:26 +0200

    Update main.c

--- main.c
+++ main.c
@@ -20,12 +20,8 @@
 
 void print_usage() {
     printf(
-    		"Usage: videop2proxy --ip CAMERA_IP --token CAMERA_HEX_TOKEN [...] \n"
+    		"Usage: videop2proxy --id CAMERA_P2P_ID --user AV_USERNAME --pass AV_PASSWORD [...] \n"
     		"\n"
-    		"Options:\n"
-    		"  --ip IP             [Required] Camera ip address.\n"
-    		"  --token HEX_TOKEN   [Required] Camera miio token.\n"
-			"\n"
     		"Modes:\n"
 #ifdef ENABLE_RTSP
 			"  --rtsp PORT         Enable RTSP server.\n"
@@ -34,61 +30,14 @@ void print_usage() {
     );
 }
 
-int tryConnect(char* ip, char* token) {
-	char cmd[256];
-	snprintf(cmd, sizeof cmd,
-		"python3 -c \""
-		"import miio;"
-		"result = miio.device.Device('%s', '%s').send('get_ipcprop', ['all']);"
-		"print(result['p2p_id']);"
-		"print(result['avID']);"
-		"print(result['avPass']);"
-        "\""
-	, ip, token);
-
-	FILE *fp;
-	char line[1035];
-	fp = popen(cmd, "r");
-
-	if (fp == NULL) {
-		DPRINTF("Failed to run command: %s\n", cmd);
-		return 1;
-	}
-
-	char p2p_id[1035];
-	char username[1035];
-	char password[1035];
-	for(int i = 1; i <= 3; i = i + 1 ){
-		if (fgets(line, sizeof(line), fp) == NULL) {
-			DPRINTF("Can't read output line %d from command: %s\n", i, cmd);
-			DPRINTF("Error connecting to camera, make sure ip and token are correct.\n");
-			return 1;
-		}
-		line[strcspn(line, "\n")] = 0;
-		switch(i) {
-			case 1:
-				strncpy(p2p_id, line, sizeof(line));
-				break;
-			case 2:
-				strncpy(username, line, sizeof(line));
-				break;
-			case 3:
-				strncpy(password, line, sizeof(line));
-				break;
-		}
-	}
-	pclose(fp);
-
-	return clientRun(p2p_id, username, password);
-}
-
 int main(int argc, char *argv[]) {
-    char *ip = "", *token = "";
+    char *id = "", *user = "", *pass = "";
 	MODE_RTSP = -1, MODE_STDOUT = -1;
 
     static struct option long_options[] = {
-        {"ip",     required_argument, 0,  'i' },
-        {"token",  required_argument, 0,  't' },
+        {"id",     required_argument, 0,  'i' },
+        {"user",     required_argument, 0,  'u' },
+        {"pass",  required_argument, 0,  'p' },
 		#ifdef ENABLE_RTSP
 		{"rtsp",   required_argument, 0,  'r' },
 		#endif
@@ -98,12 +47,14 @@ int main(int argc, char *argv[]) {
 
 	int opt= 0;
     int long_index =0;
-    while ((opt = getopt_long(argc, argv,"i:t:rs",
+    while ((opt = getopt_long(argc, argv,"i:u:p:rs",
                    long_options, &long_index )) != -1) {
         switch (opt) {
-             case 'i' : ip = optarg;
+             case 'i' : id = optarg;
                  break;
-             case 't' : token = optarg;
+             case 'u' : user = optarg;
+                 break;
+             case 'p' : pass = optarg;
                  break;
              case 'r' : MODE_RTSP = atoi(optarg);
                  break;
@@ -148,7 +99,7 @@ int main(int argc, char *argv[]) {
 	int delay = 10;
     while (1)
 	{
-		tryConnect(ip, token);
+		clientRun(id, user, pass);
 		DPRINTF("Error, waiting %d seconds and trying again.\n", delay);
 		sleep(delay);
 	}
